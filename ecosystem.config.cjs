c6766d5856e5ba6191e7c9ae83c6eb4879edea09
Ivan Mourazos <Ivan-Mourazos@users.noreply.github.com>
2026-07-02 13:52:45 +0200

    fix: usar exec_mode fork en PM2 en vez de cluster

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,10 +4,10 @@ module.exports = {
       name: 'materiales-ot',
       script: 'src/server.js',
       cwd: '/webs/materiales-ot',
+      exec_mode: 'fork',
       env: {
         NODE_ENV: 'production'
       },
-      instances: 1,
       autorestart: true,
       max_restarts: 10,
       restart_delay: 3000,
